22be40b94c56af42d0458f7e22e935879244c693
Marielle Antoine <marielle010495@gmail.com>
2016-05-27 14:19:12 +0200

    presentation de pierre

---
+++ acceuil.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Moi</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<h1>Aubrion Pierre</h1>
+	<p>c'est moi,pierre aubrion ,je galere un peu sur git</p>
+</body>
+</html>
